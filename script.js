3bc824cd4b68be7f7152384136c0424f22666c71
RahimAbbasov <8ualxm3@code.edu.az>
2024-08-03 18:39:44 +0400

    uptade

---
+++ script.js
@@ -0,0 +1,56 @@
+let cardsContainer = document.querySelector(".cardsContainer");
+async function getData() {
+  try {
+    let response = await fetch("http://localhost:3000/data");
+    if (!response.ok) {
+      throw new Error("Network response was not okay");
+    }
+    let data = await response.json();
+    data = data.services;
+    console.log(data);
+    return data;
+  } catch (error) {
+    if (error instanceof ReferenceError) {
+      console.error("Reference error occured", error);
+    } else if (error instanceof TypeError) {
+      console.error("Type error occured", error);
+    } else {
+      console.error("An error occured", error);
+    }
+  }
+}
+async function showData() {
+  try {
+    let data = await getData();
+    data.map((element) => {
+      let card = document.createElement("div");
+      card.classList.add("card");
+      card.innerHTML += `
+                   <div>
+              <img src=${element.cover_img.publicUrl} alt="" />
+              <div class="imgText">
+                <h2>${element.title_en}</h2>
+                <div class="downRow">
+                  <img src="./images/image 1019.png" alt="" />
+                  <p>Starts from 10$/h</p>
+                </div>
+              </div>
+            </div>
+            <div class="card-body">
+            ${element.subServices.map((elem) => {
+              return `<p>${elem.title_en}</p>`
+            }).join('')}
+            </div>`;
+      cardsContainer.append(card);
+    });
+  } catch (error) {
+    if (error instanceof ReferenceError) {
+      console.error("Reference error occured", error);
+    } else if (error instanceof TypeError) {
+      console.error("Type error occured", error);
+    } else {
+      console.error("An error occured", error);
+    }
+  }
+}
+showData();
